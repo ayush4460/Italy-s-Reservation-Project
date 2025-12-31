74fad2e8b3cc8384f864ce8f422e4248e41a4f8d
Ayush4460 <ayushgzala@gmail.com>
2025-12-31 06:05:50 +0530

    feat: Implement Next.js configuration with security headers and Content Security Policy.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -28,7 +28,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self'; connect-src 'self' http://localhost:5000;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self'; connect-src 'self' http://localhost:5000; https://italys-reservation.onrender.com;",
           },
         ],
       },
