9602b7c1831c5e3a4cf7716f2fff7851c11cfd18
Ayush4460 <ayushgzala@gmail.com>
2026-01-27 22:30:53 +0530

    fix: added migration command

---
+++ backend/prisma/migrations/20260127000000_add_missing_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Reservation" ADD COLUMN "startTime" TEXT;
+ALTER TABLE "Reservation" ADD COLUMN "endTime" TEXT;
+ALTER TABLE "Reservation" ADD COLUMN "cancellationReason" TEXT;
+ALTER TABLE "Reservation" ADD COLUMN "notificationType" TEXT;
